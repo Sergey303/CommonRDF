3e60c76194f0941344cb6e3dcc71462593d5f1ee
Alexander Marchuk <alexander.g.marchuk@gmail.com>
2013-09-17 12:37:09 +0200

    Параллельная разработка GraphDBmy.cs

--- Program.cs
+++ Program.cs
@@ -8,16 +8,21 @@ namespace CommonRDF
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("Start");
             DateTime tt0 = DateTime.Now;
-            Graph gr = new Graph();
+            //Graph gr = new Graph();
+            //gr.Load(@"..\..\0001.xml");
+            //gr.Test();
+            GraphDBmy grdb = new GraphDBmy(@"..\..\");
+            //grdb.Load();
+            grdb.Test();
             Console.WriteLine("Graph ok duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
-            gr.Load(@"..\..\0001.xml");
+            return;
             Console.WriteLine("Graph ok duration=" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
-            gr.Test();
             Console.WriteLine("Test ok duration========================" + (DateTime.Now - tt0).Ticks / 10000L); tt0 = DateTime.Now;
 
-            MagProgram mprog = new MagProgram(gr);
-            mprog.Run();
+            //MagProgram mprog = new MagProgram(gr);
+            //mprog.Run();
             //LeshProgram l = new LeshProgram(gr);
             //l.Run();
         }
